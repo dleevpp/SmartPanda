f529fe79f5029e3f1d52b1120bf0ca7b0b37acb4
gggg8657 <gggg8657@gmail.com>
2021-05-31 15:36:43 +0900

    .

--- WhiteFoot/sw-eng/src/App.vue
+++ WhiteFoot/sw-eng/src/App.vue
@@ -11,6 +11,44 @@
     <hr/>
     <h2 v-bind:class="{ orange : food==='orange','not-good': food!=='orange' }">한수는 {{food}}를 좋아합니다</h2>
   </div>
+  <div id="app">
+  <v-app id="inspire">
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="white"
+        elevate-on-scroll
+        scroll-target="#scrolling-techniques-7"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Title</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-7"
+        class="overflow-y-auto"
+        max-height="600"
+      >
+        <v-container style="height: 1500px;">
+        </v-container>
+      </v-sheet>
+    </v-card>
+  </v-app>
+</div>
 </template>
 //화면
 

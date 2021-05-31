6fc8348b6fee9b2a76849df409d1a36fd2651e41
gggg8657 <gggg8657@gmail.com>
2021-05-31 17:32:03 +0900

    on doing

--- WhiteFoot/front-end-vuetify/src/App.vue
+++ WhiteFoot/front-end-vuetify/src/App.vue
@@ -24,6 +24,7 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+          :to="item.to"
           link
         >
           <v-list-item-icon>
@@ -44,7 +45,7 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -54,8 +55,8 @@
     data: () => ({ 
       drawer: null,
               items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'About', icon: 'mdi-help-box' },
+          { title: 'Home', icon: 'mdi-home-variant-outline', to: '/'},
+          { title: 'About', icon: 'mdi-help-box', to: '/about'},
         ],
      }),
   }
